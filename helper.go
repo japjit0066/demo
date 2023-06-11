9ba80261a774c63a55a9b81585471457b4608f6e
japjit006 <singhjapjit6@gmail.com>
2023-06-11 12:04:35 +0200

    divided in apps

--- helper.go
+++ helper.go
@@ -1,9 +1,9 @@
 package main
 
-import "strings"
+// import "strings"
 
-func ValidateUserInput(firstName string, lastName string, email string) (bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	return isValidName, isValidEmail
-}
+// func ValidateUserInput(firstName string, lastName string, email string) (bool, bool) {
+// 	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+// 	isValidEmail := strings.Contains(email, "@")
+// 	return isValidName, isValidEmail
+// }
