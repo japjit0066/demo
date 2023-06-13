eb8110184b3520f7e167604dcd0af5a3e8d4c541
japjit006 <singhjapjit6@gmail.com>
2023-06-13 10:01:38 +0200

    helo

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # golang database selling design
-this is a demo database for selling products cd and songs 
+
+this is a demo database for selling products cd and songs
 you have to add a crud if you want to make it functional
 but it works
 you can use it like this but it can get hacked
-you can use it for pactice 
+you can use it for pactice
 you can clone it our copy the code
 please
+you can customize it
+
 # enjoy and have fun
--- main.go
+++ main.go
@@ -1,227 +1,227 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"time"
+// )
 
-var yesNo string
+// var yesNo string
 
-func main() {
-	var email string
-	var noNo string
-	var companyName = "levissima"
-	var water = ""
-	var name string
-	var number int
-	var Input string
-	fmt.Println("welcome to", companyName)
-	time.Sleep(500 * time.Millisecond)
-	fmt.Println("enter your first name :")
-	fmt.Scan(&name)
-	time.Sleep(200 * time.Millisecond)
-	fmt.Println("ok enter your email please : ")
-	fmt.Scan(&email)
-	time.Sleep(200 * time.Millisecond)
-	for {
-		fmt.Println("we have three type of water")
-		time.Sleep(200 * time.Millisecond)
-		fmt.Println("flavoured  , sparkling  , natural ")
-		time.Sleep(200 * time.Millisecond)
-		fmt.Println("choose your type of water ")
-		fmt.Scan(&water)
-		switch water {
-		case "flavoured":
+// func main() {
+// 	var email string
+// 	var noNo string
+// 	var companyName = "levissima"
+// 	var water = ""
+// 	var name string
+// 	var number int
+// 	var Input string
+// 	fmt.Println("welcome to", companyName)
+// 	time.Sleep(500 * time.Millisecond)
+// 	fmt.Println("enter your first name :")
+// 	fmt.Scan(&name)
+// 	time.Sleep(200 * time.Millisecond)
+// 	fmt.Println("ok enter your email please : ")
+// 	fmt.Scan(&email)
+// 	time.Sleep(200 * time.Millisecond)
+// 	for {
+// 		fmt.Println("we have three type of water")
+// 		time.Sleep(200 * time.Millisecond)
+// 		fmt.Println("flavoured  , sparkling  , natural ")
+// 		time.Sleep(200 * time.Millisecond)
+// 		fmt.Println("choose your type of water ")
+// 		fmt.Scan(&water)
+// 		switch water {
+// 		case "flavoured":
 
-			rand.Seed(time.Now().UnixNano())
+// 			rand.Seed(time.Now().UnixNano())
 
-			fmt.Println()
-			fmt.Println("this is flavoured water")
-			time.Sleep(300 * time.Millisecond)
-			fmt.Println("do you want to buy flavoured water (yes or no) ")
-			fmt.Scan(&yesNo)
-			if yesNo == "no" {
-				fmt.Println("ok choose again")
-				continue
+// 			fmt.Println()
+// 			fmt.Println("this is flavoured water")
+// 			time.Sleep(300 * time.Millisecond)
+// 			fmt.Println("do you want to buy flavoured water (yes or no) ")
+// 			fmt.Scan(&yesNo)
+// 			if yesNo == "no" {
+// 				fmt.Println("ok choose again")
+// 				continue
 
-			} else {
-				time.Sleep(500 * time.Millisecond)
-				fmt.Println("how many bottles : ")
-				fmt.Scan(&number)
-				time.Sleep(500 * time.Millisecond)
-				fmt.Println("we will send an email to ", email)
-				time.Sleep(200 * time.Millisecond)
-				fmt.Printf("thanks for buying %v flavoured water bottles \n", number)
-				fmt.Println("heres your order code:", rand.Int())
-				time.Sleep(200 * time.Millisecond)
-				fmt.Println("see you next time ", name)
+// 			} else {
+// 				time.Sleep(500 * time.Millisecond)
+// 				fmt.Println("how many bottles : ")
+// 				fmt.Scan(&number)
+// 				time.Sleep(500 * time.Millisecond)
+// 				fmt.Println("we will send an email to ", email)
+// 				time.Sleep(200 * time.Millisecond)
+// 				fmt.Printf("thanks for buying %v flavoured water bottles \n", number)
+// 				fmt.Println("heres your order code:", rand.Int())
+// 				time.Sleep(200 * time.Millisecond)
+// 				fmt.Println("see you next time ", name)
 
-			}
-		case "sparkling":
-			var yesNo string
-			fmt.Println("this is sparkling water")
-			fmt.Println("do you want to buy sparkling water (yes or no) ")
-			fmt.Scan(&yesNo)
-			if yesNo == "no" {
-				fmt.Println("ok choose again")
-				continue
-			} else {
+// 			}
+// 		case "sparkling":
+// 			var yesNo string
+// 			fmt.Println("this is sparkling water")
+// 			fmt.Println("do you want to buy sparkling water (yes or no) ")
+// 			fmt.Scan(&yesNo)
+// 			if yesNo == "no" {
+// 				fmt.Println("ok choose again")
+// 				continue
+// 			} else {
 
-				time.Sleep(500 * time.Millisecond)
-				fmt.Println("how many bottles : ")
-				fmt.Scan(&number)
-				time.Sleep(500 * time.Millisecond)
-				fmt.Println("we will send an email to ", email)
-				time.Sleep(200 * time.Millisecond)
-				fmt.Printf("thanks for buying %v sparkling water bottles \n", number)
-				fmt.Println("heres your order code:", rand.Int())
-				time.Sleep(1200 * time.Millisecond)
-				fmt.Println("see you next time ", name)
+// 				time.Sleep(500 * time.Millisecond)
+// 				fmt.Println("how many bottles : ")
+// 				fmt.Scan(&number)
+// 				time.Sleep(500 * time.Millisecond)
+// 				fmt.Println("we will send an email to ", email)
+// 				time.Sleep(200 * time.Millisecond)
+// 				fmt.Printf("thanks for buying %v sparkling water bottles \n", number)
+// 				fmt.Println("heres your order code:", rand.Int())
+// 				time.Sleep(1200 * time.Millisecond)
+// 				fmt.Println("see you next time ", name)
 
-			}
-		case "natural":
-			var yesNo string
-			fmt.Println("this is natural water")
-			fmt.Println("do you want to buy natural water (yes or no) ")
-			fmt.Scan(&yesNo)
-			if yesNo == "no" {
-				fmt.Println("ok choose again")
-				continue
-			} else {
+// 			}
+// 		case "natural":
+// 			var yesNo string
+// 			fmt.Println("this is natural water")
+// 			fmt.Println("do you want to buy natural water (yes or no) ")
+// 			fmt.Scan(&yesNo)
+// 			if yesNo == "no" {
+// 				fmt.Println("ok choose again")
+// 				continue
+// 			} else {
 
-				time.Sleep(500 * time.Millisecond)
-				fmt.Println("how many bottles : ")
-				fmt.Scan(&number)
-				time.Sleep(500 * time.Millisecond)
-				fmt.Println("we will send an email to ", email)
-				time.Sleep(200 * time.Millisecond)
-				fmt.Printf("thanks for buying %v natural water bottles \n", number)
-				fmt.Println("heres your order code:", rand.Int())
-				time.Sleep(1200 * time.Millisecond)
+// 				time.Sleep(500 * time.Millisecond)
+// 				fmt.Println("how many bottles : ")
+// 				fmt.Scan(&number)
+// 				time.Sleep(500 * time.Millisecond)
+// 				fmt.Println("we will send an email to ", email)
+// 				time.Sleep(200 * time.Millisecond)
+// 				fmt.Printf("thanks for buying %v natural water bottles \n", number)
+// 				fmt.Println("heres your order code:", rand.Int())
+// 				time.Sleep(1200 * time.Millisecond)
 
-			}
-		}
+// 			}
+// 		}
 
-		fmt.Println("do you want to buy more water ? (yes or no)")
-		fmt.Scan(&noNo)
-		if noNo == "no" {
-			fmt.Println("ok then bye see you next time", name)
-			break
+// 		fmt.Println("do you want to buy more water ? (yes or no)")
+// 		fmt.Scan(&noNo)
+// 		if noNo == "no" {
+// 			fmt.Println("ok then bye see you next time", name)
+// 			break
 
-		} else {
-			fmt.Println("do you want to use the same email that is ", email, "(yes or no)")
-			fmt.Scan(&Input)
-			if Input == "no" {
-				fmt.Println("please enter your email :")
-				fmt.Scan(&email)
-				fmt.Println("we will use this email for your order", email)
-				continue
-			} else {
-				for {
+// 		} else {
+// 			fmt.Println("do you want to use the same email that is ", email, "(yes or no)")
+// 			fmt.Scan(&Input)
+// 			if Input == "no" {
+// 				fmt.Println("please enter your email :")
+// 				fmt.Scan(&email)
+// 				fmt.Println("we will use this email for your order", email)
+// 				continue
+// 			} else {
+// 				for {
 
-					fmt.Println("we have three type of water")
-					time.Sleep(200 * time.Millisecond)
-					fmt.Println("flavoured  , sparkling  , natural ")
-					time.Sleep(200 * time.Millisecond)
-					fmt.Println("choose your type of water ")
-					fmt.Scan(&water)
-					switch water {
-					case "flavoured":
+// 					fmt.Println("we have three type of water")
+// 					time.Sleep(200 * time.Millisecond)
+// 					fmt.Println("flavoured  , sparkling  , natural ")
+// 					time.Sleep(200 * time.Millisecond)
+// 					fmt.Println("choose your type of water ")
+// 					fmt.Scan(&water)
+// 					switch water {
+// 					case "flavoured":
 
-						rand.Seed(time.Now().UnixNano())
+// 						rand.Seed(time.Now().UnixNano())
 
-						fmt.Println()
-						fmt.Println("this is flavoured water")
-						time.Sleep(300 * time.Millisecond)
-						fmt.Println("do you want to buy flavoured water (yes or no) ")
-						fmt.Scan(&yesNo)
-						if yesNo == "no" {
-							fmt.Println("ok choose again ")
-							continue
-						} else {
-							time.Sleep(500 * time.Millisecond)
-							fmt.Println("how many bottles : ")
-							fmt.Scan(&number)
-							time.Sleep(500 * time.Millisecond)
-							fmt.Println("we will send an email to ", email)
-							time.Sleep(200 * time.Millisecond)
-							fmt.Printf("thanks for buying %v flavoured water bottles \n", number)
-							fmt.Println("heres your order code:", rand.Int())
-							time.Sleep(200 * time.Millisecond)
-							fmt.Println("see you next time ", name)
+// 						fmt.Println()
+// 						fmt.Println("this is flavoured water")
+// 						time.Sleep(300 * time.Millisecond)
+// 						fmt.Println("do you want to buy flavoured water (yes or no) ")
+// 						fmt.Scan(&yesNo)
+// 						if yesNo == "no" {
+// 							fmt.Println("ok choose again ")
+// 							continue
+// 						} else {
+// 							time.Sleep(500 * time.Millisecond)
+// 							fmt.Println("how many bottles : ")
+// 							fmt.Scan(&number)
+// 							time.Sleep(500 * time.Millisecond)
+// 							fmt.Println("we will send an email to ", email)
+// 							time.Sleep(200 * time.Millisecond)
+// 							fmt.Printf("thanks for buying %v flavoured water bottles \n", number)
+// 							fmt.Println("heres your order code:", rand.Int())
+// 							time.Sleep(200 * time.Millisecond)
+// 							fmt.Println("see you next time ", name)
 
-						}
-					case "sparkling":
-						var yesNo string
-						fmt.Println("this is sparkling water")
-						fmt.Println("do you want to buy sparkling water (yes or no) ")
-						fmt.Scan(&yesNo)
-						if yesNo == "no" {
-							fmt.Println("ok choose again ")
-							continue
-						} else {
-							time.Sleep(500 * time.Millisecond)
-							fmt.Println("how many bottles : ")
-							fmt.Scan(&number)
-							time.Sleep(500 * time.Millisecond)
-							fmt.Println("we will send an email to ", email)
-							time.Sleep(200 * time.Millisecond)
-							fmt.Printf("thanks for buying %v sparkling water bottles \n", number)
-							fmt.Println("heres your order code:", rand.Int())
-							time.Sleep(1200 * time.Millisecond)
-							fmt.Println("see you next time ", name)
+// 						}
+// 					case "sparkling":
+// 						var yesNo string
+// 						fmt.Println("this is sparkling water")
+// 						fmt.Println("do you want to buy sparkling water (yes or no) ")
+// 						fmt.Scan(&yesNo)
+// 						if yesNo == "no" {
+// 							fmt.Println("ok choose again ")
+// 							continue
+// 						} else {
+// 							time.Sleep(500 * time.Millisecond)
+// 							fmt.Println("how many bottles : ")
+// 							fmt.Scan(&number)
+// 							time.Sleep(500 * time.Millisecond)
+// 							fmt.Println("we will send an email to ", email)
+// 							time.Sleep(200 * time.Millisecond)
+// 							fmt.Printf("thanks for buying %v sparkling water bottles \n", number)
+// 							fmt.Println("heres your order code:", rand.Int())
+// 							time.Sleep(1200 * time.Millisecond)
+// 							fmt.Println("see you next time ", name)
 
-						}
-					case "natural":
-						var yesNo string
-						fmt.Println("this is natural water")
-						fmt.Println("do you want to buy natural water (yes or no) ")
-						fmt.Scan(&yesNo)
-						if yesNo == "no" {
-							fmt.Println("ok choose again ")
-							continue
-						} else {
-							var email string
-							time.Sleep(500 * time.Millisecond)
-							fmt.Println("how many bottles : ")
-							fmt.Scan(&number)
-							time.Sleep(500 * time.Millisecond)
-							fmt.Println("we will send an email to ", email)
-							time.Sleep(200 * time.Millisecond)
-							fmt.Printf("thanks for buying %v natural water bottles \n", number)
-							fmt.Println("heres your order code:", rand.Int())
-							time.Sleep(1200 * time.Millisecond)
-						}
-					}
-					fmt.Println("do you want to buy more water ? (yes or no)")
-					fmt.Scan(&noNo)
-					if noNo == "no" {
-						break
-					} else {
+// 						}
+// 					case "natural":
+// 						var yesNo string
+// 						fmt.Println("this is natural water")
+// 						fmt.Println("do you want to buy natural water (yes or no) ")
+// 						fmt.Scan(&yesNo)
+// 						if yesNo == "no" {
+// 							fmt.Println("ok choose again ")
+// 							continue
+// 						} else {
+// 							var email string
+// 							time.Sleep(500 * time.Millisecond)
+// 							fmt.Println("how many bottles : ")
+// 							fmt.Scan(&number)
+// 							time.Sleep(500 * time.Millisecond)
+// 							fmt.Println("we will send an email to ", email)
+// 							time.Sleep(200 * time.Millisecond)
+// 							fmt.Printf("thanks for buying %v natural water bottles \n", number)
+// 							fmt.Println("heres your order code:", rand.Int())
+// 							time.Sleep(1200 * time.Millisecond)
+// 						}
+// 					}
+// 					fmt.Println("do you want to buy more water ? (yes or no)")
+// 					fmt.Scan(&noNo)
+// 					if noNo == "no" {
+// 						break
+// 					} else {
 
-						fmt.Println("do you want to use the same email that is ", email, "(yes or no)")
-						fmt.Scan(&Input)
-						if Input == "no" {
-							fmt.Println("please enter your email :")
-							fmt.Scan(&email)
-							fmt.Println("we will use this email for your order", email)
-							continue
-						}
+// 						fmt.Println("do you want to use the same email that is ", email, "(yes or no)")
+// 						fmt.Scan(&Input)
+// 						if Input == "no" {
+// 							fmt.Println("please enter your email :")
+// 							fmt.Scan(&email)
+// 							fmt.Println("we will use this email for your order", email)
+// 							continue
+// 						}
 
-					}
-				}
-				fmt.Println("are you sure (yes or no)")
-				fmt.Scan(&Input)
-				if Input == "yes" {
-					fmt.Println("ok bye see you next time")
-					break
-				}
-				if Input == "no" {
-					fmt.Println("okay then lets continue")
-					continue
-				}
-			}
-		}
-	}
-}
+// 					}
+// 				}
+// 				fmt.Println("are you sure (yes or no)")
+// 				fmt.Scan(&Input)
+// 				if Input == "yes" {
+// 					fmt.Println("ok bye see you next time")
+// 					break
+// 				}
+// 				if Input == "no" {
+// 					fmt.Println("okay then lets continue")
+// 					continue
+// 				}
+// 			}
+// 		}
+// 	}
+// }
