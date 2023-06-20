d01184cf3169868696232d8b5d7f9244f98a753d
japjit006 <singhjapjit6@gmail.com>
2023-06-20 16:13:40 +0200

    label

---
+++ label.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+iForLoop:
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100; j++ {
+			if j == 30 {
+				break iForLoop
+			}
+			fmt.Printf("%d%d ", i, j)
+		}
+		fmt.Println()
+	}
+}
